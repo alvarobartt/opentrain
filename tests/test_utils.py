4e81a625278c4164468baad628f5f1a9ab813825
Alvaro Bartolome <alvarobartt@yahoo.com>
2023-04-20 11:05:59 +0200

    🧪 Add `prepare/validate_openai_dataset` tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,10 @@
-from opentrain.utils import list_fine_tunes
+import json
+
+from opentrain.utils import (
+    list_fine_tunes,
+    prepare_openai_dataset,
+    validate_openai_dataset,
+)
 
 
 def test_list_fine_tunes():
@@ -6,3 +12,25 @@ def test_list_fine_tunes():
     assert isinstance(fine_tunes, list)
     assert len(fine_tunes) > 0
     assert isinstance(fine_tunes[0], str)
+
+
+def test_prepare_openai_dataset():
+    data = [{"prompt": "Hello", "completion": "World"}]
+    output_path = prepare_openai_dataset(data)
+    assert isinstance(output_path, str)
+
+
+def test_validate_openai_dataset():
+    data = [{"prompt": "Hello", "completion": "World"}]
+    with open("test.jsonl", "w") as f:
+        for entry in data:
+            json.dump(entry, f)
+            f.write("\n")
+    assert validate_openai_dataset("test.jsonl")
+
+    data = [{"not_prompt": "Hello", "not_completion": "World"}]
+    with open("test.jsonl", "w") as f:
+        for entry in data:
+            json.dump(entry, f)
+            f.write("\n")
+    assert validate_openai_dataset("test.jsonl") is False

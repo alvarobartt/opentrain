28a02bf479959cde4de0c108c0b9192ab3433006
Alvaro Bartolome <alvarobartt@yahoo.com>
2023-04-30 21:31:37 +0200

    ✨ Add `Inference` class

--- src/opentrain/__init__.py
+++ src/opentrain/__init__.py
@@ -4,5 +4,12 @@ __author__ = "Alvaro Bartolome <alvarobartt@gmail.com>"
 __version__ = "0.1.0"
 
 from opentrain.dataset import Dataset, File, list_datasets, list_files
+from opentrain.inference import Inference
 
-__all__ = ["Dataset", "File", "list_datasets", "list_files"]
+__all__ = [
+    "Dataset",
+    "File",
+    "list_datasets",
+    "list_files",
+    "Inference",
+]

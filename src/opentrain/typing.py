2d46c9496136a2151ad69e5048a05a0bd250635c
Alvaro Bartolome <alvarobartt@yahoo.com>
2023-04-30 22:04:13 +0200

    ✏️ Add both `str` & `dict` types to `DatasetType`

--- src/opentrain/typing.py
+++ src/opentrain/typing.py
@@ -2,4 +2,6 @@ from typing import Dict, Union
 
 from opentrain.dataset import Dataset, File
 
-DatasetType = Union[Dataset, Dict[str, Dataset], File, Dict[str, File]]
+DatasetType = Union[
+    str, Dict[str, str], Dataset, Dict[str, Dataset], File, Dict[str, File]
+]
